3cdb8ba9a74b7c6d119bef49933d537c43c02962
MalinaC27 <malina.chis.75@gmail.com>
2026-03-04 13:27:20 +0200

    am adaugatfunctie bubble sort si cautare secventiala

--- SDA/service/car_service.py
+++ SDA/service/car_service.py
@@ -14,3 +14,66 @@ class CarService:
 
     def get_all_cars(self):
         return self.__car_repository.find_all()
+
+    def sort_secondary(self, i1, j1, criteriu):
+        cars = self.__repository.get_all_cars()
+
+        if criteriu == "profit":
+            profit_i = cars[i1].pretVanzare - cars[i1].pretAchizitie
+            profit_j = cars[j1].pretVanzare - cars[j1].pretAchizitie
+
+            if profit_i > profit_j:
+                cars[i1], cars[j1] = cars[j1], cars[i1]
+                return 1
+            return 0
+
+        else:
+            key = lambda obj: getattr(obj, criteriu)
+
+            if key(cars[i1]) > key(cars[j1]):
+                cars[i1], cars[j1] = cars[j1], cars[i1]
+                return 1
+
+        return 0
+
+def sort_all_cars(self, criterii):  # BubbleSort - avand ordin de complexitate O(n^2)
+    cars = self.__repository.get_all_cars()
+    n = len(cars)
+    n1 = len(criterii)
+    ok = 0
+    if criterii[0] == "profit":
+        key1 = lambda obj: getattr(obj, "pretAchizitie")
+        key2 = lambda obj: getattr(obj, "pretVanzare")
+
+    else:
+        key = lambda obj: getattr(obj, criterii[0])
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if criterii[0] == "profit":
+                cond = (key2(cars[j]) - key1(cars[j])) > (key2(cars[j + 1]) - key1(cars[j + 1]))
+                cond1 = (key2(cars[j]) - key1(cars[j])) == (key2(cars[j + 1]) - key1(cars[j + 1]))
+                if cond == True:
+                    cars[j], cars[j + 1] = cars[j + 1], cars[j]
+            else:
+                if key(cars[j]) > key(cars[j + 1]):
+                    cars[j], cars[j + 1] = cars[j + 1], cars[j]
+                if key(cars[j]) == key(cars[j + 1]) and len(criterii) > 1:
+                    for k in range(1, n1):
+                        valoare = self.sort_secondary(j, j + 1, criterii[k])
+                        if valoare == 1:
+                            break
+    return cars
+
+
+def search(self, valoare):
+    cars = self.__repository.get_all_cars()
+    criterii = ["marca", "model", "token", "pretAchizitie", "pretVanzare"]
+
+    results = []
+
+    for criteriu in criterii:
+        for car in cars:
+            if getattr(car, criteriu) == valoare:
+                results.append(car)
+
+    return results
