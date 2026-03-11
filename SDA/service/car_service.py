86d24dc4ff5821ec6615be89cfae5e168201f005
malinabochis <malina.bochis@bestcj.ro>
2026-03-11 17:56:04 +0200

    wrote generic algorithm merge_sort for sorting with nlogn complexity and comparators that merge_sort can use

--- SDA/service/car_service.py
+++ SDA/service/car_service.py
@@ -1,6 +1,5 @@
 from SDA.domain.entities import Car
 
-
 class CarService:
     def __init__(self, car_repository):
         self.__car_repository = car_repository
@@ -14,3 +13,53 @@ class CarService:
 
     def get_all_cars(self):
         return self.__car_repository.find_all()
+
+    def cmp_token(a, b):
+        if a.token < b.token:
+            return -1
+        elif a.token > b.token:
+            return 1
+        else:
+            return 0
+
+    def cmp_marca_model(a, b):
+        if a.marca < b.marca:
+            return -1
+        elif a.marca > b.marca:
+            return 1
+        else:
+            if a.model < b.model:
+                return -1
+            elif a.model > b.model:
+                return 1
+            else:
+                return 0
+
+    def cmp_marca_model_token(a, b):
+        if a.marca < b.marca:
+            return -1
+        elif a.marca > b.marca:
+            return 1
+        else:
+            if a.model < b.model:
+                return -1
+            elif a.model > b.model:
+                return 1
+            else:
+                if a.token < b.token:
+                    return -1
+                elif a.token > b.token:
+                    return 1
+                else:
+                    return 0
+
+    def cmp_profit(a, b):
+        profit_a = a.pret_vanz - a.pret_ach
+        profit_b = b.pret_vanz - b.pret_ach
+
+        if profit_a < profit_b:
+            return -1
+        elif profit_a > profit_b:
+            return 1
+        else:
+            return 0
